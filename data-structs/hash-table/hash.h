6bcfc019dd24a914a23dcb0f7db9c4d6f7f76195
wadamiru <wadamiru@gmail.com>
2026-03-10 15:08:11 +1100

    hash table header file implementation: done

--- data-structs/hash-table/hash.h
+++ data-structs/hash-table/hash.h
@@ -38,7 +38,7 @@ int hash (map_t* map, const char* key) {
   return hash % map->cap;
 }
 
-int resize_&_rehash (map_t* map) { // return 1 if success, 0 if not
+int resize_n_rehash (map_t* map) { // return 1 if success, 0 if not
   int cap0 = map->cap;
   item_t** bucket0 = map->bucket;
   // double capacity
@@ -65,4 +65,70 @@ int resize_&_rehash (map_t* map) { // return 1 if success, 0 if not
   }
   free(bucket0); // free odl bucket
   return 1;
+}
+
+void insert (map_t* map, char* key, char* val) {
+  item_t* newitm = (item_t*)malloc(sizeof(item_t));
+  set_item(newitm, key, val);
+  if (map->size==map->cap) {
+    int s = resize_n_rehash(map);
+    if (!s) {
+      perror("[ERROR] insertion failed --resize_n_rehash\n");
+      return;
+    }
+  }
+  int idx = hash(map, key);
+  if (map->bucket[idx]==NULL) {
+    map->bucket[idx] = newitm;
+  }
+  else {
+    newitm->next = map->bucket[idx];
+    map->bucket[idx] = newitm;
+  }
+  map->size++;
+}
+
+void delete (map_t* map, char* key) {
+  int idx = hash(map, key);
+  item_t* prev = NULL;
+  item_t* curr = map->bucket[idx];
+  while (curr) {
+    if (strcmp(key, curr->key)==0) {
+      if (curr==map->bucket[idx]) {
+        map->bucket[idx] = curr->next;
+      }
+      else {
+        prev->next = curr->next;
+      }
+      free(curr);
+      break;
+    }
+    prev = curr;
+    curr = curr->next;
+  }
+}
+
+char* search (map_t* map, char* key) {
+  int idx = hash(map, key);
+  item_t* curr = map->bucket[idx];
+  while (curr) {
+    if (strcmp(key, curr->key)==0) {
+      return curr->val;
+    }
+    curr = curr->next;
+  }
+  perror("[ERROR] no key exists (%s)\n", key);
+  return NULL;
+}
+
+void print_map (map_t* map) {
+  for (int i=0; i<map->size; i++) {
+    printf("[%d]", i);
+    item_t* curr = map->bucket[i];
+    while (curr) {
+      printf("(%s, %s) -> ", curr->key, curr->val);
+      curr = curr->next;
+    }
+    printf("NULL\n");
+  }
 }
